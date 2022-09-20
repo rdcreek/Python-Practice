cb969d6f237f0fd4f45418b76adb256aac762423
Richard Creek <108111800+rdcreek@users.noreply.github.com>
2022-09-20 16:47:18 -0400

    Update rpsls.py

--- RockPaperScissorsLizardSpock/rpsls.py
+++ RockPaperScissorsLizardSpock/rpsls.py
@@ -26,7 +26,30 @@ def name_to_number(name):
         return "scissors"
     else:
         return "Error"
-
+    
+def rpsls(player_choice):
+    print("")
+    print("Player chooses {}".format(player_choice)) #The platform wouldn't work with f-strings.  Very annoying.  
+    playernum = name_to_number(player_choice)
+    compnum = random.randrange(4)   
+    compchoice = number_to_name(compnum)
+    print("Computer chooses {}".format(compchoice))
+    diff = (playernum - compnum) % 5
+    if diff == 1 or diff == 2:
+        print("Player Wins!")
+    elif diff > 2:
+        print("Computer Wins!")
+    else:
+        print("Player and Computer Tie")
+    
+    
+    
+          
+rpsls("rock")
+rpsls("Spock")
+rpsls("paper")
+rpsls("lizard")
+rpsls("scissors")
 
 
 
