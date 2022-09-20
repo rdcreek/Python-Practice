d608b66bfadd1136c4f4fc947a2db1bfe7258bce
Richard Creek <108111800+rdcreek@users.noreply.github.com>
2022-09-20 16:14:07 -0400

    Create rpsls.py

---
+++ RockPaperScissorsLizardSpock/rpsls.py
@@ -0,0 +1,32 @@
+
+def name_to_number(name):
+    if name == "rock":
+        return 0
+    elif name == "Spock":
+        return 1
+    elif name == "paper":
+        return 2
+    elif name == "lizard":
+        return 3
+    elif name == "scissors":
+        return 4
+    else:
+        return "Error"
+  
+  def number_to_name(num):
+    if num == 0:
+        return "rock"
+    elif num == 1:
+        return "Spock"
+    elif num == 2:
+        return "paper"
+    elif num == 3:
+        return "lizard"
+    elif num == 4:
+        return "scissors"
+    else:
+        return "Error"
+
+
+
+
